925649d06683e2541a813b930ad52ffdcb2feb72
S-hossain09 <sahilhossain1429@gmail.com>
2026-03-25 15:05:57 +0600

    feat: add JSON file reader with error handling

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+// const command = process.argv.slice(2);
+
+type Statuses = 'done' | 'todo' | 'in-progress';
+
+/**
+ * These values that will be stored inside the json file.
+ */
+interface Task {
+    id: number;
+    status: Statuses;
+    description: string;
+    createdAt: string;
+    updatedAt: string;
+};
+
+const FILE_PATH = path.join(process.cwd(), "tasks.json");
+
+
+/**
+ * Executes a function and returns its result.
+ * if the function throws, logs the error and returns the fallback value instead.
+ * 
+ * @param fn - The function to execute
+ * @param fallback - The value to return if `fn` throws
+ * @returns - The result of `fn`, or `fallback` on error.
+ */
+function tryCatch<T>(fn: () => T, fallback: T): T {
+    try {
+        return fn();
+    } catch (error) {
+        console.error("Operation Failed: ", error);
+        return fallback;
+    }
+}
+
+/**
+ * Read values from json file and returns the values.
+ * 
+ * @returns - The Task from the json file.
+ */
+function loadTasks(): Task[] {
+    if(!fs.existsSync(FILE_PATH))
+        fs.writeFileSync(FILE_PATH,"[]", "utf-8");
+
+    return tryCatch(() => {
+        const raw = fs.readFileSync(FILE_PATH, "utf-8");
+        return JSON.parse(raw) as Task[];
+    }, []);
+}
